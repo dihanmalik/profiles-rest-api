4cae6577a8246460d10e19e1f5d5d5e68e50aa53
dihanmalik <abdulmaliknahid@gmail.com>
2020-12-21 18:18:20 +0800

    feat: added put, patch, delete method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
+
+class HelloAPIView(APIView):
+    """HELLO API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        data = [
+            'data1',
+            'data2',
+            'data3'
+        ]
+
+        return Response({'message': 'OK', 'list': data})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
